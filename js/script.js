772da4ee046697b7d97f055d28d47aada99779a2
mahmud035 <mahamudulhasan4148@gmail.com>
2022-08-21 23:29:36 +0600

    display player name when SELECT button clicked

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const players = [];
+
+function addToCart(element) {
+  const playerName =
+    element.previousElementSibling.previousElementSibling.innerText;
+
+  const playerObject = {
+    name: playerName,
+  };
+
+  players.push(playerObject);
+  displayPlayerName(players);
+}
+
+function displayPlayerName() {
+  const playerNameContainer = document.getElementById(
+    'selected-player-container'
+  );
+  playerNameContainer.innerHTML = '';
+
+  for (const player of players) {
+    const li = document.createElement('li');
+    li.classList.add('ms-4');
+    li.style.fontSize = '1.5rem';
+    li.innerHTML = player.name;
+    playerNameContainer.appendChild(li);
+  }
+}
